2b71159e7da3e181496e699df30bdee3f474021e
hollowspecter <tigervb@aol.com>
2015-10-14 18:34:03 +0200

    until part 8

---
+++ SandsUncharted/Assets/CharacterLogic.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterLogic : MonoBehaviour
+{
+    #region Variables (private)
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private float directionDampTime = .25f; //delays animatior settings for a natural feel, goodfor pivoting
+    private float speed = 0.0f;
+    private float h = 0.0f;
+    private float v = 0.0f;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        if (anim.layerCount >= 2) {
+            anim.SetLayerWeight(1, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim) {
+            //Pull values from controller/keyboard
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+
+            speed = new Vector2(h, v).sqrMagnitude;
+
+            anim.SetFloat("Speed", speed);
+            anim.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+    }
+}
---
+++ SandsUncharted/Assets/ThirdPersonCam.cs
@@ -0,0 +1,77 @@
+﻿///<summary>
+///<copyright>(c) Vivien Baguio</copyright>
+///http://www.vivienbaguio.com
+///</summary>
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Camera script for third person controller logic
+/// </summary>
+public class ThirdPersonCam : MonoBehaviour
+{
+    #region variables (private)
+    [SerializeField]
+    private float distanceAway;
+    [SerializeField]
+    private float distanceUp;
+    [SerializeField]
+    private float smooth;
+    [SerializeField]
+    private Transform follow;
+    private Vector3 targetPosition;
+    #endregion
+
+    #region Properties (public)
+
+    #endregion
+
+    #region Unity event functions
+
+    ///<summary>
+    ///Use this for very first initialization
+    ///</summary>
+    void Awake()
+    {
+
+    }
+
+    ///<summary>
+    ///Use this for initialization
+    ///</summary>
+    void Start()
+    {
+        follow = GameObject.FindWithTag("Player").transform;
+    }
+
+    ///<summary>
+    ///Debugging information should be put here
+    ///</summary>
+    void OnDrawGizmos()
+    {
+
+    }
+
+    // good for Camera stuff
+    void LateUpdate()
+    {
+        //setting the target position to be the correct offset from the target
+        targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+
+        //making smooth transition between it's current pos and the pos it want to be in
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+        //make sure the cam is looking the rightway
+        transform.LookAt(follow);
+    }
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
