f8394a40a7dd5d6f9a7707ef81feda46755e9c3a
hollowspecter <tigervb@aol.com>
2015-10-15 16:24:54 +0200

    Part10

--- SandsUncharted/Assets/CharacterLogic.cs
+++ SandsUncharted/Assets/CharacterLogic.cs
@@ -4,13 +4,27 @@ using System.Collections;
 public class CharacterLogic : MonoBehaviour
 {
     #region Variables (private)
+    //Inspector serialized
     [SerializeField]
     private Animator anim;
     [SerializeField]
     private float directionDampTime = .25f; //delays animatior settings for a natural feel, goodfor pivoting
+    [SerializeField]
+    private ThirdPersonCam gamecam;
+    [SerializeField]
+    private float directionSpeed = 3.0f;
+    [SerializeField]
+    private float rotationDegreePerSecond = 120f;
+
+    //private global
     private float speed = 0.0f;
-    private float h = 0.0f;
-    private float v = 0.0f;
+    private float direction = 0f;
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+    private AnimatorStateInfo stateInfo;
+
+    //Hashes
+    private int m_LocomotionId = 0;
     #endregion
 
     // Use this for initialization
@@ -21,20 +35,79 @@ public class CharacterLogic : MonoBehaviour
         if (anim.layerCount >= 2) {
             anim.SetLayerWeight(1, 1);
         }
+
+        //Hash all animation names for perforcmance
+        m_LocomotionId = Animator.StringToHash("Base Layer.Locomotion");
     }
 
     // Update is called once per frame
     void Update()
     {
         if (anim) {
-            //Pull values from controller/keyboard
-            h = Input.GetAxis("Horizontal");
-            v = Input.GetAxis("Vertical");
+            //Set animation stateInfo
+            stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 
-            speed = new Vector2(h, v).sqrMagnitude;
+            //Pull values from controller/keyboard
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+
+            //speed = new Vector2(horizontal, vertical).sqrMagnitude; OLD
+
+            //Translate controls stick coordinatesinto world/cam/character space
+            StickoWorldspace(this.transform, gamecam.transform, ref direction, ref speed);
 
             anim.SetFloat("Speed", speed);
-            anim.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+            anim.SetFloat("Direction", direction, directionDampTime, Time.deltaTime);
         }
+    }
+
+    void FixedUpdate()
+    {
+        //Rotate character model if stick is tilted right of left,
+        //but only if character is moving in that direction
+        if (IsInLocomotion() && ((direction >= 0 && horizontal >= 0) || direction < 0 && horizontal < 0)) {
+            Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, //lerping from zero rotation
+                new Vector3(0f, rotationDegreePerSecond * (horizontal < 0f ? -1f : 1f), 0f), //shifting left or right
+                Mathf.Abs(horizontal)); //based on the amount of horizontal value
+
+            Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
+            this.transform.rotation = (this.transform.rotation * deltaRotation);
+        }
+    }
+
+    public void StickoWorldspace(Transform root, Transform camera, ref float directionOut, ref float speedOut)
+    {
+        Vector3 rootDirection = root.forward;
+
+        Vector3 stickDirecttion = new Vector3(horizontal, 0, vertical);
+
+        speedOut = stickDirecttion.sqrMagnitude;
+
+        //Get camera rotation
+        Vector3 CameraDirection = camera.forward;
+        CameraDirection.y = 0.0f; //kill Y
+        Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, CameraDirection);
+
+        //Convert joystick input to Worldspace coordinates
+        Vector3 moveDirection = referentialShift * stickDirecttion;
+        Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
+
+#if UNITY_EDITOR
+        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+        //Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), stickDirecttion, Color.blue);
+#endif
+
+        float angleRootToMove = Vector3.Angle(rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+
+        angleRootToMove /= 180f; //make it a hemisphere
+
+        directionOut = angleRootToMove * directionSpeed;
+
+    }
+
+    public bool IsInLocomotion()
+    {
+        return stateInfo.nameHash == m_LocomotionId;
     }
 }
--- SandsUncharted/Assets/ThirdPersonCam.cs
+++ SandsUncharted/Assets/ThirdPersonCam.cs
@@ -2,6 +2,7 @@
 ///<copyright>(c) Vivien Baguio</copyright>
 ///http://www.vivienbaguio.com
 ///</summary>
+///TODO: change Target behaviour so the camera doesnt follow the character when holding!
 
 using UnityEngine;
 using System.Collections;
@@ -9,9 +10,11 @@ using System.Collections;
 /// <summary>
 /// Camera script for third person controller logic
 /// </summary>
+[RequireComponent (typeof (BarsEffect))]
 public class ThirdPersonCam : MonoBehaviour
 {
     #region variables (private)
+    //Serialized private
     [SerializeField]
     private float distanceAway;
     [SerializeField]
@@ -19,11 +22,39 @@ public class ThirdPersonCam : MonoBehaviour
     [SerializeField]
     private float smooth;
     [SerializeField]
-    private Transform follow;
+    private Transform followTransform;
+    [SerializeField]
+    private float distanceFromWall = .5f;
+    [SerializeField]
+    private float wideScreen = .2f;
+    [SerializeField]
+    private float targetingTime = .5f;
+
+
+    //smoothing and damping
+    private Vector3 velocityCamSmooth = Vector3.zero;
+    [SerializeField]
+    private float camSmoothDampTime = 0.1f;
+
+
+    //private global only
+    private Vector3 lookDir;
     private Vector3 targetPosition;
+    private BarsEffect barEffect;
+    private CamStates camState = CamStates.Behind;
+    private float targetTriggerTreshhold = 0.01f;
+
     #endregion
 
     #region Properties (public)
+
+    public enum CamStates
+    {
+        Behind,
+        FirstPerson,
+        Target,
+        Free
+    }
 
     #endregion
 
@@ -42,7 +73,13 @@ public class ThirdPersonCam : MonoBehaviour
     ///</summary>
     void Start()
     {
-        follow = GameObject.FindWithTag("Player").transform;
+        followTransform = GameObject.FindWithTag("Player").transform;
+        lookDir = followTransform.forward;
+
+        barEffect = GetComponent<BarsEffect>();
+        if (barEffect == null) {
+            Debug.LogError("Attach a widescreenBarsEffect script to the camera.", this);
+        }
     }
 
     ///<summary>
@@ -56,22 +93,71 @@ public class ThirdPersonCam : MonoBehaviour
     // good for Camera stuff
     void LateUpdate()
     {
-        //setting the target position to be the correct offset from the target
-        targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
-        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
-        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
-        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+        Vector3 characterOffset = followTransform.position + new Vector3(0f, distanceUp, 0f);
+
+        //Determine camera state
+        Debug.Log(Input.GetAxis("Target"));
+
+        if (Input.GetAxis("Target") > targetTriggerTreshhold) {
+            barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, wideScreen, targetingTime);
+            camState = CamStates.Target;
+        }
+        else {
+            barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, 0f, targetingTime);
+            camState = CamStates.Behind;
+        }
+
+        switch (camState) {
+            case CamStates.Behind:
+                //Calculate direction from camera to player, kill Y, normalize to give valid direction with unit magnitude
+                lookDir = characterOffset - this.transform.position;
+                lookDir.y = 0f;
+                lookDir.Normalize();
+
+                //setting the target position to be the correct offset from the target
+                targetPosition = characterOffset + followTransform.up * distanceUp - lookDir * distanceAway;
+                break;
+            case CamStates.Target:
+                lookDir = followTransform.forward;
+                break;
+        }
+
+        targetPosition = characterOffset + followTransform.up * distanceUp - lookDir * distanceAway;
+
+
+        CompensateForWalls(characterOffset, ref targetPosition);
 
         //making smooth transition between it's current pos and the pos it want to be in
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+        SmoothPosition(this.transform.position, targetPosition);
 
         //make sure the cam is looking the rightway
-        transform.LookAt(follow);
+        transform.LookAt(followTransform);
     }
 
     #endregion
 
     #region Methods
+
+    private void SmoothPosition(Vector3 fromPos, Vector3 toPos)
+    {
+        //Making a smooth transition between camera's current position and the target position
+        this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
+    }
+
+    private void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget)
+    {
+#if UNITY_EDITOR
+        Debug.DrawLine(fromObject, toTarget, Color.cyan);
+#endif
+        //Compensate for walls between camera
+        RaycastHit wallHit = new RaycastHit();
+        if (Physics.Linecast(fromObject, toTarget, out wallHit)) {
+#if UNITY_EDITOR
+            Debug.DrawRay(wallHit.point, Vector3.left, Color.red);
+#endif
+            toTarget = new Vector3(wallHit.point.x, toTarget.y, wallHit.point.z) + wallHit.normal * distanceFromWall;
+        }
+    }
 
     #endregion
 }
