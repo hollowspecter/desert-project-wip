f01acd35cb4e8204fccfcc86ce59e5278672d819
hollowspecter <tigervb@aol.com>
2015-10-22 12:12:48 +0200

    mesh generation code fehlt noch

---
+++ SandsUncharted/Assets/Bezier.cs
@@ -0,0 +1,114 @@
+﻿///<summary>
+///<copyright>(c) Vivien Baguio</copyright>
+///http://www.vivienbaguio.com
+///</summary>
+
+using UnityEngine;
+using System.Collections;
+
+public struct OrientedPoint
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public OrientedPoint(Vector3 position, Quaternion rotation)
+    {
+        this.position = position;
+        this.rotation = rotation;
+    }
+
+    public Vector3 LocalToWorld(Vector3 point)
+    {
+        return position + rotation * point;
+    }
+
+    public Vector3 WorldToLocal(Vector3 point)
+    {
+        return Quaternion.Inverse(rotation) * (point - position);
+    }
+
+    public Vector3 LocalToWorldDirection(Vector3 dir)
+    {
+        return rotation * dir;
+    }
+}
+
+/// <summary>
+/// #DESCRIPTION OF CLASS#
+/// </summary>
+public class Bezier
+{
+    #region variables (private)
+    public Vector3[] pts;
+    #endregion
+
+    #region Properties (public)
+
+    #endregion
+
+    #region Methods
+    public OrientedPoint[] GetBezierPath()
+    {
+        OrientedPoint[] path = new OrientedPoint[4];
+
+        for (int i = 0; i < 4; i++) {
+            path[i] = new OrientedPoint();
+            path[i].position = GetPoint(pts, i * 0.25f);
+            path[i].rotation = GetOrientation3D(pts, i * 0.25f, Vector3.up);
+        }
+
+        return path;
+    }
+
+    Vector3 GetPoint(Vector3[] pts, float t)
+    {
+        float omt = 1f - t;
+        float omt2 = omt * omt;
+        float t2 = t * t;
+        return pts[0] * (omt2 * omt) +
+               pts[1] * (3f * omt2 * t) +
+               pts[2] * (3f * omt * t2) +
+               pts[3] * (t2 * t);
+    }
+
+    Vector3 GetTangent(Vector3[] pts, float t)
+    {
+        float omt = 1f - t;
+        float omt2 = omt * omt;
+        float t2 = t * t;
+        Vector3 tangent =
+            pts[0] * (-omt2) +
+            pts[1] * (3f * omt2 - 2 * omt) +
+            pts[2] * (-3f * t2 + 2 * t) +
+            pts[3] * (t2);
+        return tangent.normalized;
+    }
+
+    Vector3 GetNormal2D(Vector3[] pts, float t)
+    {
+        Vector3 tng = GetTangent(pts, t);
+        return new Vector3(-tng.y, tng.x, 0f); // rotated by 90 degrees
+    }
+
+    Vector3 GetNormal3D(Vector3[] pts, float t, Vector3 up)
+    {
+        Vector3 tng = GetTangent(pts, t);
+        Vector3 binormal = Vector3.Cross(up, tng).normalized;
+        return Vector3.Cross(tng, binormal);
+    }
+
+    Quaternion GetOrientation2D(Vector3[] pts, float t)
+    {
+        Vector3 tng = GetTangent(pts, t);
+        Vector3 nrm = GetNormal2D(pts, t);
+        return Quaternion.LookRotation(tng, nrm);
+    }
+
+    Quaternion GetOrientation3D(Vector3[] pts, float t, Vector3 up)
+    {
+        Vector3 tng = GetTangent(pts, t);
+        Vector3 nrm = GetNormal3D(pts, t, up);
+        return Quaternion.LookRotation(tng, nrm);
+    }
+    #endregion
+}
---
+++ SandsUncharted/Assets/shape2D.cs
@@ -0,0 +1,158 @@
+﻿///<summary>
+///<copyright>(c) Vivien Baguio</copyright>
+///http://www.vivienbaguio.com
+///</summary>
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// #DESCRIPTION OF CLASS#
+/// </summary>
+public class shape2D : UniqueMesh
+{
+    #region variables (private)
+    public Vector2[] verts;
+    public Vector2[] normals = new Vector2[6];
+    public float[] us;
+    public int[] lines;
+
+    private Bezier bezier;
+    #endregion
+
+    #region Properties (public)
+
+    #endregion
+
+    #region Unity event functions
+
+    ///<summary>
+    ///Use this for very first initialization
+    ///</summary>
+    void Awake()
+    {
+        normals[0] = getNormal(verts[1] - verts[0]).normalized;
+        normals[1] = normals[0];
+        normals[2] = getNormal(verts[3] - verts[2]).normalized;
+        normals[3] = normals[2];
+        normals[4] = getNormal(verts[4] - verts[3]).normalized;
+        normals[5] = getNormal(verts[5] - verts[4]).normalized;
+
+        us = new float[]{
+            0f, 0.2f, 0.2f, 0.4f, 0.6f, 1.0f
+        };
+
+        lines = new int[]{
+            0,1,
+            2,3,
+            3,4,
+            4,5
+        };
+    }
+
+    ///<summary>
+    ///Use this for initialization
+    ///</summary>
+    void Start()
+    {
+        bezier = new Bezier();
+        bezier.pts = new Vector3[]{
+            new Vector3(0f,0f,0f),
+            new Vector3(0f,0f,3f),
+            new Vector3(3f,0f,3f),
+            new Vector3(3f,0f,0f)
+        };
+
+        Extrude(mesh, this, bezier.GetBezierPath());
+    }
+
+    ///<summary>
+    ///Debugging information should be put here
+    ///</summary>
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        if (verts.Length == 6) {
+            Gizmos.DrawLine(transform.position + new Vector3(verts[0].x, verts[0].y, 0), transform.position + new Vector3(verts[1].x, verts[1].y, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(verts[2].x, verts[2].y, 0), transform.position + new Vector3(verts[3].x, verts[3].y, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(verts[3].x, verts[3].y, 0), transform.position + new Vector3(verts[4].x, verts[4].y, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(verts[4].x, verts[4].y, 0), transform.position + new Vector3(verts[5].x, verts[5].y, 0));
+        }
+
+        Gizmos.color = Color.green;
+
+        Vector3 tmp = transform.position + new Vector3(verts[0].x, verts[0].y, 0);
+        Gizmos.DrawLine(tmp, tmp + new Vector3(normals[0].x, normals[0].y, 0));
+        tmp = transform.position + new Vector3(verts[1].x, verts[1].y, 0);
+        Gizmos.DrawLine(tmp, tmp + new Vector3(normals[1].x, normals[1].y, 0));
+        tmp = transform.position + new Vector3(verts[2].x, verts[2].y, 0);
+        Gizmos.DrawLine(tmp, tmp + new Vector3(normals[2].x, normals[2].y, 0));
+        tmp = transform.position + new Vector3(verts[3].x, verts[3].y, 0);
+        Gizmos.DrawLine(tmp, tmp + new Vector3(normals[3].x, normals[3].y, 0));
+        tmp = transform.position + new Vector3(verts[4].x, verts[4].y, 0);
+        Gizmos.DrawLine(tmp, tmp + new Vector3(normals[4].x, normals[4].y, 0));
+        tmp = transform.position + new Vector3(verts[5].x, verts[5].y, 0);
+        Gizmos.DrawLine(tmp, tmp + new Vector3(normals[5].x, normals[5].y, 0));
+
+        foreach (OrientedPoint p in bezier.GetBezierPath()) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(p.position, Vector3.one*0.1f);
+        }
+
+        if (mesh == null)
+            return;
+
+        foreach (Vector3 vertex in mesh.vertices) {
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(vertex, 0.05f);
+        }
+
+        Gizmos.DrawWireMesh(mesh);
+    }
+
+    #endregion
+
+    #region Methods
+    Vector2 getNormal(Vector2 tangent)
+    {
+        return new Vector2(-tangent.y, tangent.x);
+    }
+
+    public void Extrude(Mesh mesh, shape2D shape, OrientedPoint[] path)
+    {
+        // initialisation
+        int vertsInShape = shape.verts.Length;
+        int segments = path.Length - 1;
+        int edgeLoops = path.Length;
+        int vertCount = vertsInShape * edgeLoops;
+        int triCount = shape.lines.Length * segments;
+        int triIndexCount = triCount * 3;
+
+        int[] triangleIndices = new int[triIndexCount];
+        Vector3[] vertices = new Vector3[vertCount];
+        Vector3[] normals = new Vector3[vertCount];
+        Vector2[] uvs = new Vector2[vertCount];
+
+        Debug.Log("Vertices Count = " + vertCount);
+
+        // mesh generation code
+        for (int i = 0; i < path.Length; i++) {
+            int offset = i * vertsInShape;
+            for (int j = 0; j < vertsInShape; j++) {
+                int id = offset + j;
+                Debug.Log("Vertice Id: " + id);
+                vertices[id] = path[i].LocalToWorld(shape.verts[j]);
+                normals[id] = path[i].LocalToWorldDirection(shape.normals[j]);
+                uvs[id] = new Vector2(shape.us[j], i / ((float)edgeLoops));
+            }
+        }
+
+        // apply
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangleIndices;
+        mesh.normals = normals;
+        mesh.uv = uvs;
+    }
+    #endregion
+}
