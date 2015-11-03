3492ec4253f4f20e211409b4642ad73d6cffb9b7
Sushiy <rftpool36@rft3603.rz.fh-trier.de>
2015-11-03 17:42:39 +0100

    started interactable interface work

---
+++ SandsUncharted/Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private string interactionString = "Interact"; //The string to be displayed next to the button prompt
+
+    private SphereCollider coll; //The sphere trigger of this object
+
+    private InteractionManager manager; //The Interaction manager on the player
+
+    private Vector3 LookAtPosition; //The position the Camera should look at when the Object is interacted upon
+
+    private float maxInteractionAngle = 120f; //The maximum angle at which the Object can be interacted with, centered around the forward axis of the object
+
+    void Awake()
+    {
+        Init();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+
+    //Initialization method
+    void Init()
+    {
+        coll = GetComponent<SphereCollider>();
+        if (coll == null)
+        {
+            Debug.LogError("spherecollider is null");
+        }
+        manager = GameObject.FindGameObjectWithTag("Player").GetComponent<InteractionManager>();
+        if (manager == null)
+        {
+            Debug.LogError("manager is null");
+        }
+    }
+
+    //This method will hold the objects specific interaction
+    public void Interact()
+    {
+        Debug.Log("I have been interacted with");
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.CompareTag("Player") && CheckInteractionAngle())
+        {
+            manager.AddInteractable(this);
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.CompareTag("Player") && CheckInteractionAngle())
+        {
+            manager.RemoveInteractable(this);
+        }
+    }
+
+    //Check if the Angle to the player is within the Interaction angle
+    bool CheckInteractionAngle()
+    {
+        if (manager != null)
+        {
+            Vector3 flatForward = Vector3.ProjectOnPlane(transform.forward, Vector3.up);
+            Vector3 flatPlayerVector = Vector3.ProjectOnPlane((manager.transform.position - transform.position), Vector3.up);
+            float angle = Vector3.Angle(flatForward, flatPlayerVector);
+
+            if (angle <= maxInteractionAngle / 2)
+            {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    void OnDrawGizmos()
+    {
+        //Gizmos.color = CheckInteractionAngle() ? Color.green : Color.red;
+        //if(manager != null)
+            //Gizmos.DrawLine(manager.transform.position, transform.position);
+        
+    }
+}
---
+++ SandsUncharted/Assets/Scripts/Interactable/InteractionManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InteractionManager : MonoBehaviour
+{
+
+    private List<Interactable> interactables; //List containing all the Interactables that we can currently interact with
+
+    private Interactable currInteractable; //The currently selected Interactable
+
+    private Camera _cam; // a reference to the main camera
+
+	// Use this for initialization
+	void Start ()
+    {
+        interactables = new List<Interactable>();
+        //_cam = GameObject.Find("Camera").transform.FindChild("MainCamera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ChooseCurrInteractable();
+	}
+
+    public void AddInteractable(Interactable i)
+    {
+        interactables.Add(i);
+    }
+
+    public void RemoveInteractable(Interactable i)
+    {
+        interactables.Remove(i);
+    }
+
+    //Change the interactiontarget to the next or last interactable on the list
+    public void SwitchCurrInteractable(bool next)
+    {
+        int index = interactables.IndexOf(currInteractable);
+
+        //if we want the next in the list, try to get index + 1. if we are at the end, jump to the first index
+        if(next)
+        {
+            index = (index + 1) % interactables.Count;
+        }
+        //if we want the last in the list, try to get index - 1. if we are at the beginning, jump to the last index
+        else
+        {
+            index = index - 1 < 0 ? interactables.Count - 1 : index - 1;
+        }
+    }
+    //Automatically choose the "best" currInteractable based on viewing direction and distance
+    void ChooseCurrInteractable()
+    {
+        float candidateDistance = float.MaxValue;
+        float candidateAngle, maxAngle = 60f;
+        Interactable bestCandidate = null;
+
+        foreach(Interactable i in interactables)
+        {
+            float distance = Vector3.Distance(i.transform.position, transform.position);
+            float angle = Vector3.Angle(Vector3.ProjectOnPlane((i.transform.position - transform.position), Vector3.up), Vector3.ProjectOnPlane(transform.forward, Vector3.up));
+            
+            //if the angle between the cameradirection and the vector to the object is low enough and the distance is closer than the distance to the current candidate
+            if (angle < maxAngle && candidateDistance > distance)
+            {
+                candidateDistance = distance;
+                candidateAngle = angle;
+                bestCandidate = i;
+                Debug.Log("foundya");
+            }
+        }
+        currInteractable = bestCandidate;
+
+    }
+
+    void OnDrawGizmos()
+    {
+        if(currInteractable != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, currInteractable.transform.position);
+        }
+    }
+}
