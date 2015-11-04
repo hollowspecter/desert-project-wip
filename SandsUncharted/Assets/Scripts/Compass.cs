c273c5a64d7eca46bc5d2547e221b93b25260a05
Sushiy <rftpool36@rft3603.rz.fh-trier.de>
2015-11-04 14:58:28 +0100

    made compass

---
+++ SandsUncharted/Assets/Scripts/Compass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compass : MonoBehaviour
+{
+    Vector3 north = new Vector3(1f, 0f, 0f);
+
+    Vector3 heading;
+
+    Transform needle;
+
+	// Use this for initialization
+	void Start ()
+    {
+        needle = transform.Find("Needle");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Quaternion northRotation = Quaternion.LookRotation(north, transform.up);
+        needle.rotation = Quaternion.RotateTowards(needle.rotation, northRotation, 10f);
+	}
+}
