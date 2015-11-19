b92b473a40251af0cf0c1717f19334cf773c3e6e
Sushiy <julivizi@yahoo.de>
2015-11-19 16:41:38 +0100

    made meshfunctions

--- SandsUncharted/Assets/Scripts/Drawing/CatmullRomSpline.cs
+++ SandsUncharted/Assets/Scripts/Drawing/CatmullRomSpline.cs
@@ -8,23 +8,39 @@ public class CatmullRomSpline : MonoBehaviour
     /*http://www.habrador.com/tutorials/catmull-rom-splines/ */
 
     [SerializeField]
+    private GameObject renderTarget;
+
+    [SerializeField]
     private List<Transform> controlPoints;
 
     private Vector3 startControlPoint, endControlPoint;
 
     private bool isLooping = false;
 
+    private List<Vector3> vertices;
+
 	// Use this for initialization
 	void Start()
     {
         controlPoints = new List<Transform>();
+        vertices = new List<Vector3>();
+        Transform positions = transform.Find("positions");
+        for(int i = 0; i < positions.childCount; i++)
+        {
+            controlPoints.Add(positions.GetChild(i));
+        }
         CalcStartEndControlPoint();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    
+        CalcStartEndControlPoint();
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            CalcMeshVertices();
+            GenerateMesh();
+        }
 	}
 
     void OnDrawGizmos()
@@ -36,7 +52,7 @@ public class CatmullRomSpline : MonoBehaviour
         {
             Gizmos.DrawWireSphere(controlPoints[i].position, 0.3f);
         }
-
+        
         //Draw the Catmull-Rom lines between the points
         for (int i = 0; i < controlPoints.Count; i++)
         {
@@ -48,7 +64,7 @@ public class CatmullRomSpline : MonoBehaviour
                 continue;
             }
 
-            DisplayCatmullRomSpline(i);
+            DrawSplinePart(i);
 
         }
 
@@ -59,7 +75,7 @@ public class CatmullRomSpline : MonoBehaviour
     }
 
     //Calculate the Spline points of the Catmull-Rom-Spline
-    Vector3 CalculateCatmullRomSpline(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    Vector3 CalculatePosition(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
     {
         Vector3 a = 0.5f * (2f * p1);
         Vector3 b = 0.5f * (p2 - p0);
@@ -71,15 +87,18 @@ public class CatmullRomSpline : MonoBehaviour
         return pos;
     }
 
-    Vector3 CalculateDerivative(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    Vector3 CalculateTangent(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
     {
-
-
-        Vector3 tangent = Vector3.zero;
+        //catmull'(u) = 0.5 *((-p0 + p2) + 2 * (2*p0 - 5*p1 + 4*p2 - p3) * u + 3 * (-p0 + 3*p1 - 3*p2 + p3) * u * u)
+        Vector3 a = 0.5f * (p2 - p0);
+        Vector3 b = 0.5f * (2f * (2f * p0 - 5f * p1 + 4f * p2 - p3));
+        Vector3 c = 0.5f * (3 * (-p0 + 3 * p1 + p3));
+        
+        Vector3 tangent = a + (b * t) + (c * t * t);
         return tangent;
     }
 
-    void DisplayCatmullRomSpline(int pos)
+    void DrawSplinePart(int pos)
     {
         Vector3 p0, p1, p2, p3;
         //use Clampfunction to allow looping the spline
@@ -95,7 +114,7 @@ public class CatmullRomSpline : MonoBehaviour
         for (float t = 0; t < 1; t += 0.1f)
         {
             //Find the coordinates between the control points with a Catmull-Rom spline
-            Vector3 newPos = CalculateCatmullRomSpline(t, p0, p1, p2, p3);
+            Vector3 newPos = CalculatePosition(t, p0, p1, p2, p3);
 
             //Cant display anything the first iteration
             if (t == 0)
@@ -110,6 +129,13 @@ public class CatmullRomSpline : MonoBehaviour
 
         //Also draw the last line since it is always less than 1, so we will always miss it
         Gizmos.DrawLine(lastPos, p2);
+    }
+
+    //Get all controlpoints relevant to the spline where the index "pos" is the second point
+    Vector3[] GetPartControlPoints(int pos)
+    {
+        Vector3[] points = { ClampListPosition(pos - 1), ClampListPosition(pos), ClampListPosition(pos + 1), ClampListPosition(pos + 2)};
+        return points;
     }
 
     Vector3 ClampListPosition(int pos)
@@ -142,6 +168,84 @@ public class CatmullRomSpline : MonoBehaviour
             endControlPoint = controlPoints[n].position + (controlPoints[n].position - controlPoints[n - 1].position);
         }
     }
+
+    /******MESHVERTEX CALCULATION METHODS********/
+
+    void CalcMeshVertices()
+    {
+        float step = 0.125f;
+        float thickness = 0.5f;
+
+        for (int i = 0; i < controlPoints.Count; i++)
+        {
+
+            if ((i == controlPoints.Count - 1) && !isLooping)
+            {
+                continue;
+            }
+
+            Debug.Log("test");
+            Vector3[] p = GetPartControlPoints(i);
+            for (float t = 0; t <= 1.0f; t += step)
+            {
+                Vector3 point = CalculatePosition(i, p[0], p[1], p[2], p[3]);
+                Vector3 tangent = CalculateTangent(i, p[0], p[1], p[2], p[3]);
+                float len = tangent.magnitude;
+                tangent *= thickness;
+                tangent = new Vector3(-tangent.z, tangent.y, tangent.x);
+                Vector3 v1 = new Vector3();
+                v1 = point + tangent;
+                vertices.Add(v1);
+                Vector3 v2 = new Vector3();
+                v2 = point - tangent;
+                vertices.Add(v2);
+
+                if (t > 1.0f) t = 1.0f;
+            }
+        }
+    }
+
+    void GenerateMesh()
+    {
+        float width = 100f;
+        float height = 100f;
+        Mesh m = new Mesh();
+        m.name = "ScriptedMesh";
+        m.vertices = new Vector3[] {
+         new Vector3(-width, -height, 0.01f),
+         new Vector3(width, -height, 0.01f),
+         new Vector3(width, height, 0.01f),
+         new Vector3(-width, height, 0.01f)
+        };
+        m.uv = new Vector2[] {
+         new Vector2 (0, 0),
+         new Vector2 (0, 1),
+         new Vector2(1, 1),
+         new Vector2 (1, 0)
+        };
+        m.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+        m.RecalculateNormals();
+
+        renderTarget.GetComponent<MeshFilter>().mesh = m;
+        /*
+        if(vertices.Count >= 4)
+        {
+            Mesh mesh = new Mesh();
+            mesh.name = "Procedural_Spline_Mesh";
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = new int[3 * (vertices.Count - 2)];
+            for (int i = 0; i < vertices.Count - 2; i++)
+            {
+                mesh.triangles[3 * i + 0] = i;
+                mesh.triangles[3 * i + 1] = i + 1;
+                mesh.triangles[3 * i + 2] = i + 2;
+            }
+            mesh.RecalculateNormals();
+            renderTarget.GetComponent<MeshFilter>().mesh = mesh;
+        }*/
+    }
+
+    /******MESHVERTEX CALCULATION METHODS********/
 
     void AddControlPoint(Transform t)
     {
