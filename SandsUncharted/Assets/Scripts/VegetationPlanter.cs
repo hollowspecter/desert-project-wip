806622482ff76522de75d1d8003a98d2260bcfc6
hollowspecter <tigervb@aol.com>
2016-01-08 15:54:57 +0100

    Circle drawing methods work

--- SandsUncharted/Assets/Scripts/VegetationPlanter.cs
+++ SandsUncharted/Assets/Scripts/VegetationPlanter.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using UnityEditor;
 using UnityEngine.Assertions;
 using System.Collections;
+using System.Collections.Generic;
 
 public class VegetationPlanter : MonoBehaviour
 {
@@ -99,7 +100,10 @@ public class VegetationPlanter : MonoBehaviour
         // Fill in the values (get filled within range of 0 and 1)
         float[,] values;
         FillNormalizedValues(out values, ref noises);
-        
+
+        // DEBUG: Draw a circle!
+        DrawFilledCircle2(ref values, 65, 100, 20, 1f);
+
         // Apply the pixels
         for (int x = 0; x < values.GetLength(0); ++x) {
             for (int y = 0; y < values.GetLength(1); ++y) {
@@ -154,4 +158,252 @@ public class VegetationPlanter : MonoBehaviour
         }
 
     }
+
+    #region Circle Methods
+
+    /// <summary>
+    /// Draws a Circle in a 2D Square Grid
+    /// </summary>
+    /// <param name="values">The 2D Grid values (made by noise)</param>
+    /// <param name="centerX">X coordinate in the square grid of the center of the circle</param>
+    /// <param name="centerY">Y coordinate in the square grid of the center of the circle</param>
+    /// <param name="radius">Radius of the circle</param>
+    /// <param name="subtrahend">The values grid beeing subtracted by the subtrahend on the line from the circle</param>
+    private void DrawCircle(ref float[,] values, int centerX, int centerY, int radius, float subtrahend)
+    {
+        int d = 3 - (2 * radius);
+        int x = 0;
+        int y = radius;
+
+        do {
+            // ensure index is in range before setting (depends on your image implementation)
+            // in this case we check if the pixel location is within the bounds of the image before setting the pixel
+            // then subtract the subtrahend
+            // last, clamp the values between 0 and 1 since they are already normalized
+            if (centerX + x >= 0 && centerX + x <= values.GetLength(0) - 1 && centerY + y >= 0 && centerY + y <= values.GetLength(1) - 1) { values[centerX + x, centerY + y] -= subtrahend; Mathf.Clamp(values[centerX + x, centerY + y], 0f, 1f);}
+            if (centerX + x >= 0 && centerX + x <= values.GetLength(0) - 1 && centerY - y >= 0 && centerY - y <= values.GetLength(1) - 1) { values[centerX + x, centerY - y] -= subtrahend; Mathf.Clamp(values[centerX + x, centerY - y], 0f, 1f);}
+            if (centerX - x >= 0 && centerX - x <= values.GetLength(0) - 1 && centerY + y >= 0 && centerY + y <= values.GetLength(1) - 1) { values[centerX - x, centerY + y] -= subtrahend; Mathf.Clamp(values[centerX - x, centerY + y], 0f, 1f);}
+            if (centerX - x >= 0 && centerX - x <= values.GetLength(0) - 1 && centerY - y >= 0 && centerY - y <= values.GetLength(1) - 1) { values[centerX - x, centerY - y] -= subtrahend; Mathf.Clamp(values[centerX - x, centerY - y], 0f, 1f);}
+            if (centerX + y >= 0 && centerX + y <= values.GetLength(0) - 1 && centerY + x >= 0 && centerY + x <= values.GetLength(1) - 1) { values[centerX + y, centerY + x] -= subtrahend; Mathf.Clamp(values[centerX + y, centerY + x], 0f, 1f);}
+            if (centerX + y >= 0 && centerX + y <= values.GetLength(0) - 1 && centerY - x >= 0 && centerY - x <= values.GetLength(1) - 1) { values[centerX + y, centerY - x] -= subtrahend; Mathf.Clamp(values[centerX + y, centerY - x], 0f, 1f);}
+            if (centerX - y >= 0 && centerX - y <= values.GetLength(0) - 1 && centerY + x >= 0 && centerY + x <= values.GetLength(1) - 1) { values[centerX - y, centerY + x] -= subtrahend; Mathf.Clamp(values[centerX - y, centerY + x], 0f, 1f);}
+            if (centerX - y >= 0 && centerX - y <= values.GetLength(0) - 1 && centerY - x >= 0 && centerY - x <= values.GetLength(1) - 1) { values[centerX - y, centerY - x] -= subtrahend; Mathf.Clamp(values[centerX - y, centerY - x], 0f, 1f);}
+            if (d < 0) {
+                d += 2 * x + 1;
+            }
+            else {
+                d += 2 * (x - y) + 1;
+                y--;
+            }
+            x++;
+        } while (x <= y);
+    }
+
+    struct cell
+    {
+        public int x;
+        public int y;
+
+        public cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    bool InRange(int x, int y)
+    {
+        return
+            x >= 0 && x < mapgen.TotalWidth &&
+            y >= 0 && y < mapgen.TotalDepth;
+    }
+
+    bool InRange(cell c)
+    {
+        return
+            c.x >= 0 && c.x < mapgen.TotalWidth &&
+            c.y >= 0 && c.y < mapgen.TotalDepth;
+    }
+
+    //cell Neighbor(cell c, int dir)
+    //{
+    //    if (dir == 0) return new cell(c.x+1, c.y);
+    //    if (dir == 1) return new cell(c.x, c.y+1);
+    //    if (dir == 2) return new cell(c.x-1, c.y);
+    //    if (dir == 3) return new cell(c.x, c.y-1);
+
+    //    Debug.LogWarning("wrong direction", this);
+    //    return new cell(0, 0);
+    //}
+
+    //private void DrawFilledCircle(ref float[,] values, int centerX, int centerY, int radius, float subtrahend, FillMode mode)
+    //{
+    //    // Initialise the Lists
+    //    cell start = new cell(centerX, centerY);
+    //    List<cell> visited = new List<cell>();
+    //    List<cell> border = new List<cell>();
+    //    List<cell> fringe = new List<cell>();
+    //    fringe.Add(start);
+    //    values[centerX, centerY] -= subtrahend;
+    //    float stepsize = 1f / (float)radius;
+
+    //    // Fill in the Border List
+    //    int d = 3 - (2 * radius);
+    //    int x = 0;
+    //    int y = radius;
+    //    do {
+    //        // ensure index is in range before setting (depends on your image implementation)
+    //        // in this case we check if the pixel location is within the bounds of the image before setting the pixel
+    //        // then subtract the subtrahend
+    //        // last, clamp the values between 0 and 1 since they are already normalized
+    //        if (InRange(centerX + x, centerY + y)) { border.Add(new cell(centerX + x, centerY + y)); }
+    //        if (InRange(centerX + x, centerY - y)) { border.Add(new cell(centerX + x, centerY - y)); }
+    //        if (InRange(centerX - x, centerY + y)) { border.Add(new cell(centerX - x, centerY + y)); }
+    //        if (InRange(centerX - x, centerY - y)) { border.Add(new cell(centerX - x, centerY - y)); }
+    //        if (InRange(centerX + y, centerY + x)) { border.Add(new cell(centerX + y, centerY + x)); }
+    //        if (InRange(centerX + y, centerY - x)) { border.Add(new cell(centerX + y, centerY - x)); }
+    //        if (InRange(centerX - y, centerY + x)) { border.Add(new cell(centerX - y, centerY + x)); }
+    //        if (InRange(centerX - y, centerY - x)) { border.Add(new cell(centerX - y, centerY - x)); }
+    //        if (d < 0) {
+    //            d += 2 * x + 1;
+    //        }
+    //        else {
+    //            d += 2 * (x - y) + 1;
+    //            y--;
+    //        }
+    //        x++;
+    //    } while (x <= y);
+
+    //    // Flood fill by filling from the center to the borders
+    //    // when a border is hit, the cell is removed from the border
+    //    for (int i = 0; i < radius-10; ++i) {
+    //        //while (border.Count > 0) {
+    //        List<cell> newfringe = new List<cell>();
+
+    //        if (mode == FillMode.GRADIENT)
+    //            subtrahend += stepsize;
+
+    //        foreach (cell c in fringe) {
+    //            for (int dir = 0; dir < 4; ++dir) {
+    //                cell neighbor = Neighbor(c, dir);
+    //                if (!visited.Contains(neighbor)) {
+    //                    if (!border.Contains(neighbor)) {
+    //                        visited.Add(neighbor);
+    //                        newfringe.Add(neighbor);
+    //                        if (InRange(neighbor))
+    //                            values[neighbor.x, neighbor.y] -= subtrahend;
+    //                    }
+    //                    else
+    //                        border.Remove(neighbor);
+    //                }
+    //            }
+    //        }
+    //        fringe.Clear();
+    //        fringe = newfringe;
+    //    }
+    //}
+
+    //private void DrawFilledCircle(ref float[,] values, int centerX, int centerY, int radius, float subtrahend)
+    //{
+    //    // Fill in the Border List
+    //    int d = 3 - (2 * radius);
+    //    int x = 0;
+    //    int y = radius;
+    //    cell center = new cell(centerX, centerY);
+    //    do {
+    //        FillHorizontalLine(ref values, new cell(centerX + x, centerY + y), new cell(centerX - x, centerY + y), center, subtrahend, radius);
+    //        FillHorizontalLine(ref values, new cell(centerX + x, centerY - y), new cell(centerX - x, centerY - y), center, subtrahend, radius);
+    //        FillHorizontalLine(ref values, new cell(centerX + y, centerY + x), new cell(centerX - y, centerY + x), center, subtrahend, radius);
+    //        FillHorizontalLine(ref values, new cell(centerX + y, centerY - x), new cell(centerX - y, centerY - x), center, subtrahend, radius);
+    //        if (d < 0) {
+    //            d += 2 * x + 1;
+    //        }
+    //        else {
+    //            d += 2 * (x - y) + 1;
+    //            y--;
+    //        }
+    //        x++;
+    //    } while (x <= y);
+    //}
+
+    //private void FillHorizontalLine(ref float[,] values, cell start, cell end, cell center, float subtrahend, int radius)
+    //{
+    //    //// We assume that end is aboce
+    //    if (end.x < start.x) {
+    //        cell tmp = start;
+    //        start = end;
+    //        end = tmp;
+    //    }
+
+    //    int distance = end.x - start.x;
+
+    //    for (int i = 0; i <= distance; ++i) {
+    //        if (InRange(start)) {
+    //            values[start.x + i, start.y] -= subtrahend * GetSubtrahendPercentage(center, start.x + i, start.y, radius);
+    //            Mathf.Clamp(values[start.x+i, start.y], 0f, 1f);
+    //        }
+    //    }
+    //}
+
+    private void DrawFilledCircle2(ref float[,] values, int centerX, int centerY, int radius, float subtrahend)
+    {
+        int x = radius;
+        int y = 0;
+        int xChange = 1 - (radius << 1);
+        int yChange = 0;
+        int radiusError = 0;
+
+        // List of all the cells in the circle
+        List<cell> cells = new List<cell>();
+        cell tmp;
+
+        // adding the cells to the list
+        while (x >= y) {
+            for (int i = centerX - x; i <= centerX + x; i++) {
+                tmp = new cell(i, centerY + y);
+                if (!cells.Contains(tmp)) cells.Add(tmp);
+                tmp = new cell(i, centerY - y);
+                if (!cells.Contains(tmp)) cells.Add(tmp);
+            }
+            for (int i = centerX - y; i <= centerX + y; i++) {
+                tmp = new cell(i, centerY + x);
+                if (!cells.Contains(tmp)) cells.Add(tmp);
+                tmp = new cell(i, centerY - x);
+                if (!cells.Contains(tmp)) cells.Add(tmp);
+            }
+
+            y++;
+            radiusError += yChange;
+            yChange += 2;
+            if (((radiusError << 1) + xChange) > 0) {
+                x--;
+                radiusError += xChange;
+                xChange += 2;
+            }
+        }
+
+        // subtracting the subtrahend once for every cell
+        foreach (cell c in cells) {
+            if (InRange(c)) {
+                values[c.x, c.y] -= subtrahend * GetSubtrahendPercentage(centerX, centerY, c.x, c.y, radius);
+            }
+        }
+    }
+
+    // Calculates the distance to the center of this circle-point so its a gradientfill
+    private float GetSubtrahendPercentage(cell center, int x, int y, float radius)
+    {
+        Vector2 centerv = new Vector2(center.x, center.y);
+        Vector2 cell = new Vector2(x, y);
+        float distance = Vector2.Distance(centerv, cell);
+
+        return 1f - (distance / radius);
+    }
+
+    private float GetSubtrahendPercentage(int centerX, int centerY, int x, int y, float radius)
+    {
+        return GetSubtrahendPercentage(new cell(centerX, centerY), x, y, radius);
+    }
+
+    #endregion
 }
